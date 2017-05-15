36f5c23681d18bdb54a3a30d7843ae5b8bdd9d7b
mballeng91 <migaballengal@unal.edu.co>
2017-05-14 19:01:05 -0500

    Feat: Code to know area working

--- Documents/app/src/main/java/com/misura/tpi/misura/Misura_main.java
+++ Documents/app/src/main/java/com/misura/tpi/misura/Misura_main.java
@@ -1,14 +1,24 @@
 package com.misura.tpi.misura;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import java.io.*;
+import android.graphics.*;
 
 public class Misura_main extends AppCompatActivity {
+
+
+    public static final double ELEMENT_AREA_DM2 = 5.53;
+    public static final double ELEMENT_R_VAL = 1;
+    public static final double ELEMENT_G_VAL = 115.5;
+    public static final double ELEMENT_B_VAL = 188.5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +32,79 @@ public class Misura_main extends AppCompatActivity {
     }
 
     public void get_image(View view) {
-        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(i, 1);
+        /*Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, 1);*/
+        final int SELECT_PHOTO = 100;
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
     }
 
 
+
+    public boolean is_element(int r, int g, int b){
+        return (r < 100 && g > 100 && b >100) ? true :  false;
+    }
+
+    public double three_rule(int valid, int element){
+        return (ELEMENT_AREA_DM2 * valid) / element;
+    }
+
+
+
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
-        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    try {
+                        InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+                        Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
 
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
+                        System.out.println("Succesfully selected");
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
+                        int imageWidth = yourSelectedImage.getWidth();
+                        int imageHeigth = yourSelectedImage.getHeight();
 
-            // String picturePath contains the path of selected Image
-        }
+                        int pix [] = new int[imageWidth*imageHeigth];
+                        yourSelectedImage.getPixels(pix, 0, imageWidth, 0, 0, imageWidth, imageHeigth);
+
+                        int valid_pixels = 0 ;
+                        int element_pixels = 0;
+                        int total_pix = imageHeigth*imageWidth;
+                        int area;
 
 
-     }
+                        for (int x = 0 ; x< imageWidth; x++){
+                            for(int y = 0; y< imageHeigth; y++){
+                                int index  = y*imageWidth+x;
+                                int r = (pix[index] >> 16) & 0xff;
+                                int g = (pix[index] >> 8) & 0xff;
+                                int b = pix[index] & 0xff;
+
+                                if (!is_element(r, g, b)){
+                                    valid_pixels++;
+                                }else{
+                                    element_pixels++;
+                                }
+                            }
+                        }
+                        System.out.println("The image has: "+ imageHeigth*imageWidth + " of resolution");
+                        System.out.println("The image has: "+ valid_pixels + " valid pixels");
+                        System.out.println("The image has: "+ element_pixels + " element pixels");
+                        System.out.println("The piece has: "+ three_rule(valid_pixels, element_pixels) + "dm2");
+                    }
+                    catch(FileNotFoundException nf){
+                        nf.printStackTrace();
+                    }
+                }
+
+    }
+
+
+
+
+
     }
