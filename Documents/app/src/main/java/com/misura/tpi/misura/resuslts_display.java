0e18543da5ad1addd5e6cdd9f466d4980c74b343
Nemaul <mabarretor@unal.edu.co>
2017-05-16 22:54:48 -0500

    Icono, imagen en pantalla de resultados

---
+++ Documents/app/src/main/java/com/misura/tpi/misura/resuslts_display.java
@@ -0,0 +1,46 @@
+package com.misura.tpi.misura;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import java.text.DecimalFormat;
+
+public class resuslts_display extends AppCompatActivity {
+
+    public double roundTwoDecimals(double to_round) {
+        DecimalFormat twoDForm = new DecimalFormat("#.##");
+        return Double.valueOf(twoDForm.format(to_round));
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resuslts_display);
+        Bundle extras = getIntent().getExtras();
+        double fetch_area = extras.getDouble("calc_area");
+        double rnd_area = roundTwoDecimals(fetch_area);
+        String disp_area = Double.toString(rnd_area);
+        TextView disp = (TextView) findViewById(R.id.textViewdata);
+        disp.setText(disp_area+"dm2");
+        Uri uri = getIntent().getParcelableExtra("imageUri");
+        ImageView viewimage = (ImageView)findViewById(R.id.imageView1);
+        viewimage.setImageURI(uri);
+
+
+
+
+        }
+
+
+
+
+
+    }
+
